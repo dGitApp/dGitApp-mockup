dce9dcd0abc80b2df2521e5481c417e2c48fb776
stemerlini <s.merlini19@imperial.ac.uk>
2022-02-26 16:02:52 +0000

    frontend login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 // React dependences
-import React, {StyleSheet} from "react";
-import { Container} from "react-bootstrap";
-import Chatbox from "./components/chatbox";
+import React from "react";
+import { Button, Container, Image} from "react-bootstrap";
 
 // SCSS files
 import "./scss/page.scss"
 
+// ASSETS
+import logo from "./assets/icons/dGitIconGreen.png"
+
 export default function App() {
     return (
         <Container>
-            <Chatbox></Chatbox>
+            <div className="page.container">
+                <div style={{flex: 1, marginBottom: 130, textAlign: "center"}}>
+                    <Image fluid src={logo} width={200} height={200}></Image>
+                </div>
+                <div style={{flex: 1, margin: 0, textAlign: "center"}}>
+                    <Button variant = "light" onClick={() => alert("connect")}>
+                        Connect Wallet
+                    </Button>
+                </div>
+            </div>
         </Container>
     )
 }
